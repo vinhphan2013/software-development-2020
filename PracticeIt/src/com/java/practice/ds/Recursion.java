0072cb43b1df98d89cf5351e263f0217d301e23e
Vinh Phan <quangvinh.g.phan@gmail.com>
2020-06-14 14:24:36 -0700

    Second problem added

--- PracticeIt/src/com/java/practice/ds/Recursion.java
+++ PracticeIt/src/com/java/practice/ds/Recursion.java
@@ -2,6 +2,30 @@ package com.java.practice.ds;
 
 public class Recursion {
     public static void main(String args[]){
-        System.out.println();
+        //System.out.println(starString(2));
+        writeNums(2);
+
+    }
+
+    public static String starString(int n){
+        if (n < 0) {
+            throw new IllegalArgumentException();
+        }
+        else if( n == 0){
+            return "*";
+        }else{
+            return starString(n-1) +  starString(n-1);
+        }
+    }
+
+    public static void writeNums(int n){
+        if(n < 1){
+            throw new IllegalArgumentException();
+        }else if(n == 1){
+            System.out.print("1");
+        }else{
+            writeNums(n-1);
+            System.out.print(", " + n); ;
+        }
     }
 }
