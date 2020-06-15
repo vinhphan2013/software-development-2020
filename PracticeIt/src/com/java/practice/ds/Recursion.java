e61c48ee4222dc4eac44de1cb184f888ab5ff930
Vinh Phan <quangvinh.g.phan@gmail.com>
2020-06-14 21:48:17 -0700

    recurrsion for today

--- PracticeIt/src/com/java/practice/ds/Recursion.java
+++ PracticeIt/src/com/java/practice/ds/Recursion.java
@@ -25,7 +25,58 @@ public class Recursion {
             System.out.print("1");
         }else{
             writeNums(n-1);
-            System.out.print(", " + n); ;
+            System.out.print(", " + n);
+        }
+    }
+
+    public static void writeSequence(int n) {
+        if (n < 1) {
+            throw new IllegalArgumentException();
+        }
+        if (n == 1) {
+            System.out.print("1");
+        } else if (n == 2) {
+            System.out.print("1 1");
+        } else {
+            int m = (n + 1) / 2;
+            System.out.print(m + " ");
+            writeSequence(n - 2);
+            System.out.print(" " + m);
+
+        }
+    }
+
+    public static void writeSquares(int n){
+        if (n < 1) {
+            throw new IllegalArgumentException();
+        }if (n == 1) {
+            System.out.print("1");
+        }else {
+            if((n%2) != 0){
+                System.out.print(n * n + ", ");
+                writeSquares(n-2);
+                System.out.print(", " + (n-1) * (n-1));
+            }else{
+                System.out.print((n-1) * (n-1));
+                if(n!=2){
+                    System.out.print(", ");
+                    writeSquares(n-2);
+                }
+                System.out.print(", " + n * n);
+            }
+        }
+    }
+    public static void writeChars(int n){
+        if (n < 1) {
+            throw new IllegalArgumentException();
+        }if (n == 1) {
+            System.out.print("*");
+        }else if (n == 2) {
+            System.out.print("**");
+        }else {
+            System.out.print("<");
+            writeChars(n-2);
+            System.out.print(">");
         }
     }
 }
